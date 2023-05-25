9ac9db604b18a788be23f9320052b54288e74df3
alistairwalsh <alistairwalsh@me.com>
2023-05-25 10:42:47 +1000

    added note

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.llms import OpenAI
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
+    # must have api-key in secrets
     llm = OpenAI(temperature=0)
     chain = ConversationChain(llm=llm)
     return chain
